fe2b56cf623fdc1b23ec65c17188125c13661c80
Sebastien Villar <me@sebastienvillar.com>
2014-08-20 17:45:17 +0200

    Add authentication verification middleware

--- src/router.js
+++ src/router.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = __dirname + '/controllers';
+var googleAPI = require('./helpers/googleCalendarAPI');
 var controllers = {
 	authenticateController: require(path + '/authenticateController'),
 	calendarController: require(path + '/calendarController')
@@ -9,9 +10,23 @@ module.exports = function(app) {
 	var unAuthenticatedRouter = express.Router();
 	var authenticatedRouter = express.Router();
 	
-	//Add middleware for authenticated routes
+	//Middleware for authentication verification
+	authenticatedRouter.use(function(req, res, next) {
+		if (!req.query.accessToken)
+			res.status(400).json({'error': 'No access token provided'});
+		else {
+			googleAPI.isTokenValid(req.query.accessToken, function(err, isValid){
+				if (err)
+					res.status(500).send(err);
+				else if (isValid)
+					next();
+				else
+					res.status(403).json({'error': 'Access token no longer valid'});
+			});
+		}
+	});
 
-	//Authenticated routes
+	//Unauthenticated routes
 	unAuthenticatedRouter.get('/authenticate', controllers.authenticateController.authenticate);
 	unAuthenticatedRouter.get('/authenticate/callback', controllers.authenticateController.authenticateCallback);
 
